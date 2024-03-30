337444f148c4417254a9507dbcc76ed630a42bce
Shaikzaheer174 <shaikzaheer174@gmail.com>
2024-03-31 02:02:50 +0530

    added snippet for better visualization of testrunner

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  // to stop auto rerun cypress
+  // watchForFileChanges: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
